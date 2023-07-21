321516eaa4ce28405549520e85348d7fbc18d1fe
Mark Raadsen <mark.raadsen@sydney.edu.au>
2023-07-21 12:08:39 +1000

    - added new constructor that makes more sense than the default one

--- src/main/java/org/goplanit/io/converter/zoning/PlanitZoningReaderFactory.java
+++ src/main/java/org/goplanit/io/converter/zoning/PlanitZoningReaderFactory.java
@@ -1,11 +1,13 @@
 package org.goplanit.io.converter.zoning;
 
+import org.goplanit.io.xml.util.PlanitXmlReaderSettings;
 import org.goplanit.network.MacroscopicNetwork;
 import org.goplanit.network.LayeredNetwork;
 import org.goplanit.utils.exceptions.PlanItException;
 import org.goplanit.utils.id.IdGroupingToken;
 import org.goplanit.xml.generated.XMLElementMacroscopicZoning;
 import org.goplanit.zoning.Zoning;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
  * Factory class for creating zoning reader in the native PLANit format
@@ -15,17 +17,36 @@ import org.goplanit.zoning.Zoning;
  */
 public class PlanitZoningReaderFactory {
   
-  /** Factory method wit all default, expected that user configures the settings afterwards to reach minimum requirements for successful parsing
+  /** Factory method with all defaults, expected that user configures the settings afterwards to reach minimum requirements for successful parsing
    *  (input dir especially)
-   *  
+   *
+   * @param referenceCrs must be provided in absence of reference network to derive it from
    * @return created PLANit zoning reader
    */
-  public static PlanitZoningReader create(){
+  public static PlanitZoningReader create(CoordinateReferenceSystem referenceCrs){
     PlanitZoningReaderSettings settings = new PlanitZoningReaderSettings();
-    MacroscopicNetwork networkToPopulate = new MacroscopicNetwork(IdGroupingToken.collectGlobalToken());
-    Zoning zoningToPopulate = new Zoning(IdGroupingToken.collectGlobalToken(), networkToPopulate.getNetworkGroupingTokenId());
-    return create(settings, networkToPopulate, zoningToPopulate);
-  }  
+
+    MacroscopicNetwork emptyReferenceNetwork = new MacroscopicNetwork(IdGroupingToken.collectGlobalToken());
+    emptyReferenceNetwork.setCoordinateReferenceSystem(referenceCrs);
+
+    Zoning zoningToPopulate = new Zoning(IdGroupingToken.collectGlobalToken(), emptyReferenceNetwork.getNetworkGroupingTokenId());
+    return create(settings, emptyReferenceNetwork, zoningToPopulate);
+  }
+
+  /** Factory method
+   *
+   * @param pathDirectory to use
+   * @param network to extract references from (if any)
+   * @return created PLANit zoning reader
+   */
+  public static PlanitZoningReader create(
+      final String pathDirectory, final LayeredNetwork<?,?> network){
+    return create(
+        pathDirectory,
+        PlanitXmlReaderSettings.DEFAULT_XML_EXTENSION,
+        network,
+        new Zoning(network.getIdGroupingToken(), network.getNetworkGroupingTokenId()));
+  }
 
   
   /** Factory method
@@ -38,7 +59,7 @@ public class PlanitZoningReaderFactory {
    */
   public static PlanitZoningReader create(
       final String pathDirectory, final String xmlFileExtension, final LayeredNetwork<?,?> network, final Zoning zoning){
-    return create(new PlanitZoningReaderSettings(pathDirectory, xmlFileExtension),network, zoning);
+    return create(new PlanitZoningReaderSettings(pathDirectory, xmlFileExtension), network, zoning);
   }
 
   /** Factory method
